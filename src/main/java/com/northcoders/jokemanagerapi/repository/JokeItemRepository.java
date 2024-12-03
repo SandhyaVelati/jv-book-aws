147c7b6f1585fda426131a1916d21d7307f4a3d3
Sandhya Velati <120228542+SandhyaVelati@users.noreply.github.com>
2024-12-03 16:44:50 +0000

    Delete src/main/java/com/northcoders/jokemanagerapi directory

--- src/main/java/com/northcoders/jokemanagerapi/repository/JokeItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.northcoders.jokemanagerapi.repository;
-
-import com.northcoders.jokemanagerapi.model.Joke;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JokeItemRepository  extends CrudRepository<Joke, Long> {
-}
